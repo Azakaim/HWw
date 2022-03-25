dcdef87d4dfa33bb14803f7f61d7afe0f03bbc80
Azakaim <91306580+Azakaim@users.noreply.github.com>
2022-03-25 19:31:37 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,34 +32,29 @@ namespace HomeWork6
         static string UserData()
         {
             string UserData = String.Empty;
-            for (int i = 0; i < 7; i++)
+            for (int i = 0; i < 6; i++)
             {
                 if (i == 0)
                 {
-                    Console.WriteLine("Введите Id");
-                    UserData += ($"#{Console.ReadLine()}#");
+                    UserData += ($"#{DateTime.Now}#");
                 }
                 else if (i == 1)
-                {
-                    UserData += ($"{DateTime.Now}#");
-                }
-                else if (i == 2)
                 {
                     Console.WriteLine("Введите Ф. И. О.");
                     UserData += ($"{Console.ReadLine()}#");
                 }
-                else if (i == 3)
+                else if (i == 2)
                 {
                     Console.WriteLine("Введите возраст");
                     UserData += ($"{Console.ReadLine()}#");
                 }
-                else if (i == 4)
+                else if (i == 3)
                 {
                     Console.WriteLine("Введите рост");
                     
                     UserData += ($"{Console.ReadLine()}#");
                 }
-                else if (i == 5)
+                else if (i == 4)
                 {
                     Console.WriteLine("Введите дату рождения");
                     UserData += ($"{Console.ReadLine()}#");
@@ -87,6 +82,7 @@ namespace HomeWork6
             }
             else
             {
+                //id - в этой системе будет порядкоовый номер сотрудника внесенный в файл
                 string count = Convert.ToString(File.ReadAllLines(path).Length + 1);
                 File.AppendAllText(path,$"\r\n{count}{UserData()}" + Environment.NewLine);
             }
@@ -108,7 +104,7 @@ namespace HomeWork6
             }
             else
             {
-                Console.WriteLine("Нет фала для чтения!");
+                Console.WriteLine("Нет файла для чтения!");
                 Chois();
             }
         }
